8e8a07197c64ea6b4164a5b1b27a3d787d7303cf
Roshni1998 <roshnimali847@gmail.com>
2021-07-14 15:02:22 +0530

    Update the salary and sync it with Database using JDBC Prepared Statement

---
+++ src/main/java/com/bridgelabz/EmployeePayrollFileIOService.java
@@ -0,0 +1,15 @@
+package com.bridgelabz;
+
+import java.util.List;
+
+public class EmployeePayrollFileIOService {
+    public void writeData(List<EmployeePayrollData> employeePayrollList) {
+        
+    }
+
+    public void printData() {
+    }
+
+    public void countEntries() {
+    }
+}
