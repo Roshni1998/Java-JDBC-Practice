9e9e613e00669f0db7c71ea296933e6e2cefc5b2
Roshni1998 <roshnimali847@gmail.com>
2021-07-14 23:30:32 +0530

    Test Case to add a new Employee to the payroll

---
+++ src/test/java/com/bridgelabz/EmployeePayrollServiceTest.java
@@ -0,0 +1,75 @@
+package com.bridgelabz;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+
+import static com.bridgelabz.EmployeePayrollService.IOService.FILE_IO;
+import static com.bridgelabz.EmployeePayrollService.IOService.DB_IO;
+
+public class EmployeePayrollServiceTest {
+
+    @Test
+    public void given3EmployeesWhenWrittenToFileShouldMatchEmployeeEntries() {
+        EmployeePayrollData[] arraysOfEmployees = {
+                new EmployeePayrollData(1, "Jeff Bezos", 100000.0),
+                new EmployeePayrollData(2, "Bill Gates", 200000.0),
+                new EmployeePayrollData(3, "Elon Mask", 300000.0),
+        };
+        EmployeePayrollService employeePayrollService;
+        employeePayrollService = new EmployeePayrollService(Arrays.asList(arraysOfEmployees));
+        employeePayrollService.writeEmployeePayrollData(FILE_IO);
+        employeePayrollService.printData(FILE_IO);
+        long entries = employeePayrollService.countEntries(FILE_IO);
+        Assertions.assertEquals(3, entries);
+    }
+
+    @Test
+    public void givenEmployeePayrollInDB_WhenRetrieved_ShouldMatchEmployeeCount() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(DB_IO);
+        Assertions.assertEquals(4, employeePayrollData.size());
+    }
+
+    @Test
+    public void givenNewSalaryForEmployee_WhenUpdated_ShouldSyncWithDB() {
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        List<EmployeePayrollData> employeePayrollData = employeePayrollService.readEmployeePayrollData(EmployeePayrollService.IOService.DB_IO);
+        employeePayrollService.updateEmployeeSalary("Terrisa", 2000000.00);
+        boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Terrisa");
+        Assertions.assertTrue(result);
+    }
+
+    @Test
+    public void givenDateRange_WhenRetrieved_ShouldMatchEmployeeCount(){
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        employeePayrollService.readEmployeePayrollData(DB_IO);
+        LocalDate startDate = LocalDate.of(2021,04,01);
+        LocalDate endDate = LocalDate.now();
+        List<EmployeePayrollData> employeePayrollData =
+                employeePayrollService.readEmployeePayrollForDateRange(DB_IO, startDate, endDate);
+        Assertions.assertEquals(3, employeePayrollData.size());
+    }
+
+    @Test
+    public void givenPayrollData_WhenAverageSalaryRetrievedByGender_ShouldReturnProperValue(){
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        employeePayrollService.readEmployeePayrollData(DB_IO);
+        Map<String, Double> averageSalaryByGender = employeePayrollService.readAverageSalaryByGender(DB_IO);
+        Assertions.assertTrue(averageSalaryByGender.get("M").equals(2666666.6667) && averageSalaryByGender.get("F").equals(2000000.0000));
+    }
+
+    @Test
+    public void givenNewEmployee_WhenAdded_ShouldSyncWithDB(){
+        EmployeePayrollService employeePayrollService = new EmployeePayrollService();
+        employeePayrollService.readEmployeePayrollData(DB_IO);
+        employeePayrollService.addEmployeeToPayroll("Maria", 5000000.00, LocalDate.now(), "F");
+        boolean result = employeePayrollService.checkEmployeePayrollInSyncWithDB("Maria");
+        Assertions.assertTrue(result);
+    }
+}
+
